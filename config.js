0aca1e1f939023fea6dba29d9498f4114091902f
rvvidros <rvvidroseacabamentos@gmail.com>
2026-08-19 13:52:50 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 const SUPABASE_CONFIG = {
-  url: 'https://seu-projeto.supabase.co',
-  anonKey: 'sua-chave-anon-publica'
+  url: 'https://soplizhasxiyzolgwjmy.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNvcGxpemhhc3hpeXpvbGd3am15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcxNTM0MTEsImV4cCI6MjEwMjcyOTQxMX0.3SydJMhCE-eOX-hYI7CnBPktoYGRNa6zgAA72IqMvgY'
 };
